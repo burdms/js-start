3cec7a2ff687181f0167c8d443a7dd655bee0ef0
burdms <burdms@gmail.com>
2021-04-01 12:07:43 +0200

    create: lesson 01

---
+++ script.js
@@ -0,0 +1,10 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+const mission = "Frontend web developer";
+let period;
+
+alert("Hello there!");
+
+console.log("General Kenobi! Ha-ha-ha-ha... You are a bold one.");
