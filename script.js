7bfb85a690969b4eea2fe3268a7020b976d3115c
burdms <burdms@gmail.com>
2021-04-01 22:33:23 +0200

    fix: deleted unnecessary string in script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
 let money, income, addExpenses, deposit, period;
-const mission = "Frontend web developer";
+const mission = 'Frontend web developer';
 
-alert("Hello there!");
+alert('Hello there!');
 
-console.log("General Kenobi! Ha-ha-ha-ha... You are a bold one.");
+console.log('General Kenobi! Ha-ha-ha-ha... You are a bold one.');
