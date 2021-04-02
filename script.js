9e97bfa9099210761d35f1213dbaea6f0c07c614
burdms <burdms@gmail.com>
2021-04-02 17:32:11 +0200

    create: lesson02 compulsory

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 'use strict';
 
-let money, income, addExpenses, deposit, period;
-const mission = 'Frontend web developer';
+let money = 700,
+  income = 'Freelance',
+  addExpenses = 'Rent, utilities, internet, software',
+  deposit = true,
+  mission = 3000,
+  period = 8;
 
-let string = 'new string';
+console.group('***1***');
+console.log('Typeof money: ', typeof money);
+console.log('Typeof income: ', typeof income);
+console.log('Typeof deposit: ', typeof deposit);
+console.groupEnd();
 
-alert('Hello there!');
+console.group('***2***');
+console.log('Length of addExpenses: ', addExpenses.length);
+console.groupEnd();
 
-console.log('General Kenobi! Ha-ha-ha-ha... You are a bold one.');
+console.group('***3***');
+console.log('%c“Период равен ' + period + ' месяцев”', 'font-weight: bold;');
+console.log('“Цель заработать ' + mission + ' долларов”');
+console.groupEnd();
+
+console.group('***4***');
+console.log(addExpenses.toLowerCase().split());
+console.groupEnd();
+
+let budgetDay = +money / 30;
+console.group('***5***');
+console.log(
+  'Дневной бюджет: ' + budgetDay + '. Или же ' + Math.round(budgetDay)
+);
+console.groupEnd();
