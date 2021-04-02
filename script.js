719539b7a9fb8c987d325064bd3b5d4e9799ae08
burdms <burdms@gmail.com>
2021-04-03 00:30:56 +0200

    create: one line solution

--- script.js
+++ script.js
@@ -3,34 +3,8 @@
 let num = 266219;
 let result = 0;
 
-console.log('Initial num value: ', num);
-
-if (num > 0) {
-  result = 1;
-} else if (num < 0) {
-  result = -1;
-  num = Math.abs(num);
-}
-
-num = Array.from(String(num));
-num.reduce((acc, value) => {
-  return (result = +acc * +value);
-});
-
-console.log('Result value after multiplication: ', result);
-
-result **= 3;
-
-console.log('Result value in a power of 3: ', result);
-
-if (result >= 0) {
-  console.log(
-    'Final result: ',
-    Math.floor(result / 10 ** (result.toString().length - 2))
-  );
-} else {
-  console.log(
-    'Final result: ',
-    Math.ceil(result / 10 ** (result.toString().length - 3))
-  );
-}
+console.log(
+  String(
+    Array.from(String(num)).reduce((acc, value) => +acc * +value) ** 3
+  ).slice(0, 2)
+);
