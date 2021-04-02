e1701ca4373ce75f197f4a478aa537fdee4a83a5
burdms <burdms@gmail.com>
2021-04-03 00:32:33 +0200

    fix: one line now. Trust me

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 'use strict';
 
-let num = 266219;
-let result = 0;
-
 console.log(
   String(
-    Array.from(String(num)).reduce((acc, value) => +acc * +value) ** 3
+    Array.from(String(266219)).reduce((acc, value) => +acc * +value) ** 3
   ).slice(0, 2)
 );
