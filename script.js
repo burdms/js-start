4f8265b4036abc7407fd31e424bfa3e6bae222f8
burdms <burdms@gmail.com>
2021-04-03 22:42:28 +0200

    fix: final solution

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 const num = 266219;
 
-console.log(Array.from(String(num)).reduce((acc, value) => +acc * +value));
+const array = num.toString().split('');
 
-console.log(
-  String(
-    Array.from(String(num)).reduce((acc, value) => +acc * +value) ** 3
-  ).slice(0, 2)
-);
+const multi = array.reduce((acc, value) => +acc * +value);
+console.log(multi);
+
+const power = multi ** 3;
+
+const result = power.toString().slice(0, 2);
+
+console.log(result);
