764365a14405ea881550b836552eadd2f037a504
burdms <burdms@gmail.com>
2021-04-05 21:47:58 +0200

    fix: no infinity anymore now

--- script.js
+++ script.js
@@ -13,30 +13,34 @@ const money = +prompt('Ваш месячный доход?'),
   period = Math.ceil(mission / budgetMonth),
   budgetDay = Math.floor(budgetMonth / 30);
 
-console.log('Typeof money: ', typeof money);
-console.log('Typeof income: ', typeof income);
-console.log('Typeof deposit: ', typeof deposit);
+if (budgetMonth !== 0){
+  console.log('Typeof money: ', typeof money);
+  console.log('Typeof income: ', typeof income);
+  console.log('Typeof deposit: ', typeof deposit);
 
-console.log('Length of addExpenses: ', addExpenses.length);
+  console.log('Length of addExpenses: ', addExpenses.length);
 
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
+  console.log('Период равен ' + period + ' месяцев');
+  console.log('Цель заработать ' + mission + ' рублей');
 
-console.log(addExpenses.toLowerCase().split(', '));
+  console.log(addExpenses.toLowerCase().split(', '));
 
-// Вывод в консоль из задания в уроке №3
-console.log('Бюджет на месяц: ' + budgetMonth + ' рублей');
-console.log('Цель будет достигнута за ' + period + ' месяца(-ев)');
-console.log('Дневной бюджет: ' + budgetDay + ' рублей');
+  // Вывод в консоль из задания в уроке №3
+  console.log('Бюджет на месяц: ' + budgetMonth + ' рублей');
+  console.log('Цель будет достигнута за ' + period + ' месяца(-ев)');
+  console.log('Дневной бюджет: ' + budgetDay + ' рублей');
 
-if (budgetDay >= 1200) {
-  console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay < 1200) {
-  console.log('У вас средний уровень дохода');
-} else if (budgetDay > 0 && budgetDay < 600) {
-  console.log('К сожалению, у вас уровень дохода ниже среднего');
-} else if (budgetDay === 0) { 
-  console.log('Вам нужно срочно увеличить свой заработок или снизить свои траты, чтобы двигаться к поставленной цели!');
+  if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+  } else if (budgetDay >= 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+  } else if (budgetDay > 0 && budgetDay < 600) {
+    console.log('К сожалению, у вас уровень дохода ниже среднего');    
+  } else {
+    console.log('Что-то пошло не так');
+  }
 } else {
-  console.log('Что-то пошло не так');
+  console.log('Вам нужно срочно увеличить свой заработок или снизить свои траты! Так вы ничего не накопите');
 }
+
+
