eb996adf70a7497dbd6527317c4a5a64c5a8c442
burdms <burdms@gmail.com>
2021-04-05 23:27:16 +0200

    fix: a message instead of infinite period now

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 'use strict';
 
-const money = +prompt('Ваш месячный доход?'),
+const money = +prompt('Ваш месячный доход?', "80000"),
   income = 'Freelance',
-  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартира, комуналка'),
   deposit = confirm('Есть ли у вас депозит в банке?'),
-  expenses1 = prompt('Введите обязательную статью расходов №1'),
-  amount1 = +prompt('Во сколько это обойдется? №1'),
-  expenses2 = prompt('Введите обязательную статью расходов №2'),
-  amount2 = +prompt('Во сколько это обойдется? №2'),
+  expenses1 = prompt('Введите обязательную статью расходов №1', 'Квартира'),
+  amount1 = +prompt('Во сколько это обойдется? №1', '20000'),
+  expenses2 = prompt('Введите обязательную статью расходов №2', 'Комуналка'),
+  amount2 = +prompt('Во сколько это обойдется? №2', '10000'),
   budgetMonth = money - (amount1 + amount2),
   mission = 200000,
   period = Math.ceil(mission / budgetMonth),
   budgetDay = Math.floor(budgetMonth / 30);
 
-if (budgetMonth > 0){
   console.log('Typeof money: ', typeof money);
   console.log('Typeof income: ', typeof income);
   console.log('Typeof deposit: ', typeof deposit);
 
   console.log('Length of addExpenses: ', addExpenses.length);
 
-  console.log('Период равен ' + period + ' месяцев');
   console.log('Цель заработать ' + mission + ' рублей');
-
+  
   console.log(addExpenses.toLowerCase().split(', '));
+  
+  isFinite(period) ?
+    console.log('Период равен ' + period + ' месяцев')
+    : console.log("Период невероятно большой");
+  
+  isFinite(period) ?
+    console.log('Цель будет достигнута за ' + period + ' месяца(-ев)')
+    : console.log("Цель никогда не будет достигнута");
+  
 
-  // Вывод в консоль из задания в уроке №3
   console.log('Бюджет на месяц: ' + budgetMonth + ' рублей');
-  console.log('Цель будет достигнута за ' + period + ' месяца(-ев)');
   console.log('Дневной бюджет: ' + budgetDay + ' рублей');
 
   if (budgetDay >= 1200) {
@@ -37,15 +42,9 @@ if (budgetMonth > 0){
   } else if (budgetDay > 0 && budgetDay < 600) {
     console.log('К сожалению, у вас уровень дохода ниже среднего');    
   } else if (budgetDay === 0) {
-    console.log("Вашы расходы практически идентичны вашим доходам");
     console.log('Кажется, такими темпами вы будете очень долго копить ' + mission + ' рублей');
   } else {
     console.log('Что-то пошло не так');
   }
-} else if (budgetMonth === 0) {
-  console.log('Вам нужно срочно увеличить свой заработок или снизить свои траты! Так вы ничего не накопите');
-} else {
-  console.log("Выши расходы значительно превышают ваши доходы. Нужно срочно что-то менять!");
-}
 
 
