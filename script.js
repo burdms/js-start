ef36c432c021d92d54048c6bd08ce0eb2686b735
burdms <burdms@gmail.com>
2021-04-06 00:52:58 +0200

    fix: code formatting

--- script.js
+++ script.js
@@ -13,38 +13,38 @@ const money = +prompt('Ваш месячный доход?', "80000"),
   period = Math.ceil(mission / budgetMonth),
   budgetDay = Math.floor(budgetMonth / 30);
 
-  console.log('Typeof money: ', typeof money);
-  console.log('Typeof income: ', typeof income);
-  console.log('Typeof deposit: ', typeof deposit);
+console.log('Typeof money: ', typeof money);
+console.log('Typeof income: ', typeof income);
+console.log('Typeof deposit: ', typeof deposit);
 
-  console.log('Length of addExpenses: ', addExpenses.length);
+console.log('Length of addExpenses: ', addExpenses.length);
 
-  console.log('Цель заработать ' + mission + ' рублей');
-  
-  console.log(addExpenses.toLowerCase().split(', '));
-  
-  isFinite(period) ?
-    console.log('Период равен ' + period + ' месяцев')
-    : console.log("Период невероятно большой");
-  
-  isFinite(period) ?
-    console.log('Цель будет достигнута за ' + period + ' месяца(-ев)')
-    : console.log("Цель никогда не будет достигнута");
-  
+console.log('Цель заработать ' + mission + ' рублей');
 
-  console.log('Бюджет на месяц: ' + budgetMonth + ' рублей');
-  console.log('Дневной бюджет: ' + budgetDay + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
 
-  if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-  } else if (budgetDay >= 600 && budgetDay < 1200) {
-    console.log('У вас средний уровень дохода');
-  } else if (budgetDay > 0 && budgetDay < 600) {
-    console.log('К сожалению, у вас уровень дохода ниже среднего');    
-  } else if (budgetDay === 0) {
-    console.log('Кажется, такими темпами вы будете очень долго копить ' + mission + ' рублей');
-  } else {
-    console.log('Что-то пошло не так');
-  }
+isFinite(period) ?
+  console.log('Период равен ' + period + ' месяцев')
+  : console.log("Период невероятно большой");
+
+isFinite(period) ?
+  console.log('Цель будет достигнута за ' + period + ' месяца(-ев)')
+  : console.log("Цель никогда не будет достигнута");
+
+
+console.log('Бюджет на месяц: ' + budgetMonth + ' рублей');
+console.log('Дневной бюджет: ' + budgetDay + ' рублей');
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay > 0 && budgetDay < 600) {
+  console.log('К сожалению, у вас уровень дохода ниже среднего');    
+} else if (budgetDay === 0) {
+  console.log('Кажется, такими темпами вы будете очень долго копить ' + mission + ' рублей');
+} else {
+  console.log('Что-то пошло не так');
+}
 
 
