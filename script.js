34aa646247aef365b5112f54347552245e852f02
burdms <burdms@gmail.com>
2021-04-05 11:57:44 +0200

    create: lesson03

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 'use strict';
 
-let money = 700,
+const money = +prompt('Ваш месячный доход?'),
   income = 'Freelance',
-  addExpenses = 'Rent, utilities, internet, software',
-  deposit = true,
-  mission = 3000,
-  period = 8,
-  budgetDay = money / 30;
+  addExpenses = prompt(
+    'Перечислите возможные расходы за рассчитываемый период через запятую'
+  ),
+  deposit = Boolean(
+    prompt('Есть ли у вас депозит в банке? Если нет — оставьте поле пустым')
+  ),
+  expenses1 = prompt('Введите обязательную статью расходов №1'),
+  expenses2 = prompt('Введите обязательную статью расходов №2'),
+  amount1 = +prompt('Во сколько это обойдется? №1'),
+  amount2 = +prompt('Во сколько это обойдется? №2'),
+  budgetMonth = amount1 + amount2,
+  mission = 200000,
+  period = Math.ceil(mission / (money - budgetMonth)),
+  budgetDay = Math.floor(budgetMonth / 30);
 
-console.group('***1***');
 console.log('Typeof money: ', typeof money);
 console.log('Typeof income: ', typeof income);
 console.log('Typeof deposit: ', typeof deposit);
-console.groupEnd();
 
-console.group('***2***');
 console.log('Length of addExpenses: ', addExpenses.length);
-console.groupEnd();
 
-console.group('***3***');
-console.log('%c“Период равен ' + period + ' месяцев”', 'font-weight: bold;');
-console.log('“Цель заработать ' + mission + ' долларов”');
-console.groupEnd();
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
 
-console.group('***4***');
 console.log(addExpenses.toLowerCase().split(', '));
-console.groupEnd();
 
-console.group('***5***');
-console.log(
-  'Дневной бюджет: ' + budgetDay + '. Или же ' + Math.round(budgetDay)
-);
-console.groupEnd();
+// Вывод в консоль из задания в уроке №3
+console.log('Бюджет на месяц: ' + budgetMonth + ' рублей');
+console.log('Цель будет достигнута за ' + period + ' месяца(-ев)');
+console.log('Дневной бюджет: ' + budgetDay + ' рублей');
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay < 600) {
+  console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что-то пошло не так');
+}
