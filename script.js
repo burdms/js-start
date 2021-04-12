01397a675df49a6a3afa5fb4b1e5d7b0df6af478
burdms <burdms@gmail.com>
2021-04-12 23:29:25 +0200

    some code adjustments

--- script.js
+++ script.js
@@ -1,66 +1,50 @@
 'use strict';   
 
-function currentTimeA() {
+function getDate(){
     const date = new Date(),
         options = {
           weekday: 'long',
           year: 'numeric',
           month: 'long',
           day: 'numeric',
-        },
-        getTimeNoun = (number, var1, var2, var3) => {
-          if (number >= 5 && number <= 20) {
-            return var3;
-          }
-
-          number %= 10;
-          if (number === 1) {
-            return var1;
-          }
-          if (number >= 2 && number <= 4) {
-            return var2;
-          }
-
-          return var3;
         };
     
-    let hours = date.getHours(),
-        minutes = date.getMinutes(),
-        seconds = date.getSeconds(),
-        hourWord = getTimeNoun(hours, 'час', 'часа', 'часов'),
-        minuteWord = getTimeNoun(minutes, 'минута', 'минуты', 'минут'),
-        secondWord = getTimeNoun(seconds, 'секунда', 'секунды', 'секунд'),
-        resultString,
-        timeString,
-        dateString = date.toLocaleString('ru-RU', options);
+    (function () {
+        const getTimeNoun = (number, var1, var2, var3) => {
+              if (number >= 5 && number <= 20) {
+                return var3;
+              }
 
-    dateString = dateString.charAt(0).toUpperCase() + dateString.slice(1).replace('.', 'ода');
-    
-    if (hours < 10) {
-      hours = '0' + hours;
-    }
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
-    if (seconds < 10) {
-      seconds = '0' + seconds;
-    }
+              number %= 10;
+              if (number === 1) {
+                return var1;
+              }
+              if (number >= 2 && number <= 4) {
+                return var2;
+              }
 
-    timeString = `${hours} ${hourWord} ${minutes} ${minuteWord} ${seconds} ${secondWord}`;
+              return var3;
+            }, 
+            hours = date.getHours(),
+            minutes = date.getMinutes(),
+            seconds = date.getSeconds(),
+            hourWord = getTimeNoun(hours, 'час', 'часа', 'часов'),
+            minuteWord = getTimeNoun(minutes, 'минута', 'минуты', 'минут'),
+            secondWord = getTimeNoun(seconds, 'секунда', 'секунды', 'секунд'),
+            timeString = `${hours} ${hourWord} ${minutes} ${minuteWord} ${seconds} ${secondWord}`;
 
-    resultString = `Сегодня ${dateString}, ${timeString}`;
+        let dateString = date.toLocaleString('ru-RU', options);
 
-    document.getElementById('js-date_a').innerHTML = resultString;
+        dateString = dateString.charAt(0).toUpperCase() + dateString.slice(1).replace('.', 'ода');
+
+        document.getElementById('js-date_a').textContent = `Сегодня ${dateString}, ${timeString}`;
+    }());
+
+    (function() {
+
+      document.getElementById('js-date_b').textContent = date.toLocaleString('ru-RU').replace(',', ' - ');
+    }());
 }
 
-function currentTimeB() {
-  const date = new Date();
-  let resultString = date.toLocaleString('ru-RU').replace(',', ' - ');
+setInterval(getDate, 1000);
 
-  document.getElementById('js-date_b').innerHTML = resultString;
-}
-
-setInterval(() => {
-  currentTimeA();
-  currentTimeB();
-}, 1000);
