395234c47e72830cb5c6711e1ba7b5cd30a8e770
burdms <burdms@gmail.com>
2021-04-12 20:05:38 +0200

    create: lesson09_optional

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
-'use strict';
+'use strict';   
 
-const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-  today = new Date();
+function currentTime() {
+  const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    monthes = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'],
+    date = new Date();
 
-let list = '';
+  let outputA = 'Сегодня ', 
+    outputB,
+    hourWord,
+    minuteWord,
+    secondWord,
+    hours = date.getHours(),
+    minutes = date.getMinutes(),
+    seconds = date.getSeconds(),
+    day = date.getDate(),
+    month = date.getMonth() + 1,
+    year = date.getFullYear(),
+    getTimeNoun = (number, var1, var2, var3) => {
+      if (number >= 5 && number <= 20) {
+        return var3;
+      }
 
-week.forEach((item, index) => {
-  let temp = item,
-    dateIndex = index + 1;
-  if (index === 6) {
-    dateIndex = 0;
-  }
+      number %= 10;
+      if (number === 1) {
+        return var1;
+      }
+      if (number >= 2 && number <= 4) {
+        return var2;
+      }
 
-  if (dateIndex === today.getDay()){
-    temp = (`<b>${temp}</b>`);
-  }
-  
-  if (item === 'Sunday' || item === 'Saturday'){
-    temp = (`<i>${temp}</i>`);
-  } 
-  
-  if (index === week.length - 1){
-    list += temp;
-  } else {
-    list += `${temp}<br>`;
-  }
-});
+      return var3;
+    };
 
-document.querySelector('#weeklist').innerHTML = list;
+    outputA += `${days[date.getDay()]}, ${date.getDate()} ${date.getMonth()} ${year} года, `;
+
+    hourWord = getTimeNoun(hours, 'час', 'часа', 'часов');
+    minuteWord = getTimeNoun(minutes, 'минута', 'минуты', 'минут');
+    secondWord = getTimeNoun(seconds, 'секунда', 'секунды', 'секунд');
+
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    if (day < 10) {
+      day = '0' + day;
+    }
+    if (month < 10) {
+      month = '0' + month;
+    }
+
+    outputA += `${hours} ${hourWord} ${minutes} ${minuteWord} ${seconds} ${secondWord}`;
+    outputB = `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
+    
+    document.getElementById('js-date_a').innerHTML = outputA;
+    document.getElementById('js-date_b').innerHTML = outputB;
+}
+
+setInterval(currentTime, 1000);
