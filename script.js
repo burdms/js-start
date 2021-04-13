1db910b35ebce093a54f227f7349e44d96097d7a
burdms <burdms@gmail.com>
2021-04-13 15:08:25 +0200

    lesson10 p.3 & p.4

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 'use strict';
 
-const books = document.querySelectorAll('.book');
+const books = document.querySelectorAll('.book'),
+    thirdBookTitleText = books[4].querySelector('h2 > a'),
+    adv = document.querySelector('.adv');
 
 books[0].before(books[1]);
 books[3].before(books[4]);
 books[5].after(books[2]);
 
 document.body.style.backgroundImage = 'url(image/you-dont-know-js.jpg)';
+
+thirdBookTitleText.textContent = `Книга 3. this и Прототипы Объектов`;
+
+adv.remove();
